e33a2f65c8bf26ea4de745fab327ef448b1eea4a
Ezequiel M Izaguirre <ezequielmizaguirre@gmail.com>
2026-06-30 12:05:29 -0300

    feat: add FabPosition and ChatAssistantMode enums

---
+++ src/main/java/com/flowingcode/vaadin/addons/chatassistant/model/ChatAssistantMode.java
@@ -0,0 +1,29 @@
+/*-
+ * #%L
+ * Chat Assistant Add-on
+ * %%
+ * Copyright (C) 2023 - 2024 Flowing Code
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.flowingcode.vaadin.addons.chatassistant.model;
+
+/**
+ * The display mode of the chat assistant: {@link #MOBILE} opens the chat window as a full-screen
+ * dialog, while {@link #DESKTOP} opens it as an anchored popover.
+ */
+public enum ChatAssistantMode {
+  MOBILE,
+  DESKTOP;
+}
